19ab0c47586f3ea9190646b512c9816b731dd3bf
Colin Howald <colin.howald@zalando.de>
2016-04-14 12:15:11 +0200

    Token invalidation and token parsing

--- src/authentication-service.js
+++ src/authentication-service.js
@@ -1,95 +1,103 @@
-angular.module('sc-authentication', [])
-    .factory('solutionCenterAuth', [
-        '$q',
-        '$localStorage',
-        '$cookies',
-        'solutionCenterEnvironments',
-        '$location',
-        '$http',
-        function ($q, $localStorage, $cookies, environments, $location, $http) {
+angular.module('sc-authentication', ['angular-jwt'])
+  .factory('solutionCenterAuth', [
+    '$q',
+    '$localStorage',
+    '$cookies',
+    'solutionCenterEnvironments',
+    '$location',
+    '$http',
+    'jwtHelper',
+    function ($q, $localStorage, $cookies, environments, $location, $http, jwtHelper) {
 
-            'use strict';
+      'use strict';
 
-            var service = {
-                authenticate: authenticate,
-                redirectToLogin: redirectToLogin,
-                getToken: getToken,
-                logout: logout,
-                parseToken: parseToken,
-                getUser: getUser
-            };
+      var service = {
+        authenticate: authenticate,
+        redirectToLogin: redirectToLogin,
+        getToken: getToken,
+        logout: logout,
+        parseToken: parseToken,
+        getUser: getUser
+      };
 
-            return service;
+      return service;
 
-            //////////////////////
+      //////////////////////
 
-            var TOKEN_COOKIE_KEY = "SC_TOKEN";
+      var TOKEN_COOKIE_KEY = "SC_TOKEN";
 
-            function authenticate(environment, redirectUrl) {
-                var token = isAuthenticated();
+      function authenticate(environment, redirectUrl) {
+        var token = isAuthenticated();
 
-                if (!token) {
-                    redirectToLogin(environment, redirectUrl);
-                }
-
-                $localStorage.sc_token = token;
-                $localStorage.sc_user = getUserFromToken(token);
-
-                return token;
-            }
-
-            function redirectToLogin(environment, redirectUrl) {
-                $location.url = environments.getLoginUrl(environment) + "?redirect=" + redirectUrl;
-            }
-
-            function logout(environment) {
-                $localStorage.sc_token = null;
-                $localStorage.sc_user = null;
-
-                $location.url = environments.getLogoutUrl(environment); // XXX Cookies is reset in our app
-            }
-
-            function getToken() {
-                return $localStorage.sc_token || $cookies.get(TOKEN_COOKIE_KEY);
-            }
-
-            function getUser() {
-                return $localStorage.sc_user;
-            }
-
-            /*
-             PRIVATE METHODS
-             */
-
-            /**
-             * Returns a valid token in case the user is authenticated or null in case there's no user
-             * @returns {*}
-             */
-            function isAuthenticated() {
-                var token = getToken();
-                return validateToken(token);
-            }
-
-            /*
-             XXX Returns either null or a valid token
-             */
-            function validateToken(token) {
-                if (token === null) {
-                    return null;
-                }
-
-                // $http.
-
-                // TODO Call GET /tokens
-            }
-
-            function getUserFromToken(token) {
-                var parts = token.split('.');
-
-                var base64Data = decodeURIComponent(escape(atob(parts[1])));
-                var user = angular.fromJson(base64Data);
-
-                return user;
-            }
+        if (!token) {
+          redirectToLogin(environment, redirectUrl);
         }
-    ]);
+
+        $localStorage.sc_token = token;
+        $localStorage.sc_user = getUserFromToken(token);
+
+        return token;
+      }
+
+      function redirectToLogin(environment, redirectUrl) {
+        $location.url = environments.getLoginUrl(environment) + "?redirect=" + redirectUrl;
+      }
+
+      function logout(environment) {
+        $localStorage.sc_token = null;
+        $localStorage.sc_user = null;
+
+        $location.url = environments.getLogoutUrl(environment); // XXX Cookies is reset in our app
+      }
+
+      function getToken() {
+        return $localStorage.sc_token || $cookies.get(TOKEN_COOKIE_KEY);
+      }
+
+      function getUser() {
+        return $localStorage.sc_user;
+      }
+
+      /*
+       PRIVATE METHODS
+       */
+
+      /**
+       * Returns a valid token in case the user is authenticated or null in case there's no user
+       * @returns {*}
+       */
+      function isAuthenticated() {
+        var token = getToken();
+        return validateToken(token).then(
+          function () {
+            // TODO remove - should never happen?
+            return token;
+          },
+          function (response) {
+            // TODO test this
+            if (response.status === 304) {
+              return token;
+            }
+            if (response.status === 409) {
+              return response.data;
+            }
+            return null;
+          });
+      }
+
+      /*
+       XXX Returns either null or a valid token
+       */
+      function validateToken(token) {
+        if (token === null) {
+          return $q.reject("null token");
+        }
+
+        return $http.get(environments.getTokensAPI(), token);
+      }
+
+      function getUserFromToken(token) {
+        return jwtHelper.decodeToken(token);
+      }
+    }
+  ]);
--- src/environments-service.js
+++ src/environments-service.js
@@ -1,48 +1,49 @@
 angular.module('sc-authentication', [])
-    .factory('environments', [
-        function() {
-            var environments = {
-                PRODUCTION: {
-                    name: 'PRODUCTION',
-                    url: 'solutions.zalando.com',
-                    tokenservice: 'https://token-management.norris.zalan.do'
-                },
-                INTEGRATION: {
-                    name: 'INTEGRATION',
-                    url: 'usf-integration.norris.zalan.do',
-                    tokenservice: 'https://tm-integration.norris.zalan.do'
-                },
-                STAGING: {
-                    name: 'STAGING',
-                    url: 'usf-stage.norris.zalan.do',
-                    tokenservice: 'https://tm-stage.norris.zalan.do'
-                },
-                DEVELOPMENT: {
-                    name: 'DEVELOPMENT',
-                    url: 'usf-dev.norris.zalan.do',
-                    tokenservice: 'https://tm-dev-ext.norris.zalan.do'
-                },
-                LOCAL: {
-                    name: 'LOCAL',
-                    url: 'localhost',
-                    tokenservice: 'https://tm-dev-ext.norris.zalan.do'
-                }
-            };
-
-            function getUrl(environment) {
-
-            }
-
-            function getLoginUrl (environment) {
-
-            }
-
-            function getLogourUrl (environment) {
-
-            }
-
-            function getTokensEndpointUrl (environment) {
-
-            }
+  .factory('environments', [
+    function () {
+      var environments = {
+        PRODUCTION: {
+          url: 'solutions.zalando.com',
+          tokenservice: 'https://token-management.norris.zalan.do'
+        },
+        INTEGRATION: {
+          url: 'usf-integration.norris.zalan.do',
+          tokenservice: 'https://tm-integration.norris.zalan.do'
+        },
+        STAGING: {
+          url: 'usf-stage.norris.zalan.do',
+          tokenservice: 'https://tm-stage.norris.zalan.do'
+        },
+        DEVELOPMENT: {
+          url: 'usf-dev.norris.zalan.do',
+          tokenservice: 'https://tm-dev-ext.norris.zalan.do'
+        },
+        LOCAL: {
+          url: 'localhost',
+          tokenservice: 'https://tm-dev-ext.norris.zalan.do'
         }
-        ]);
+      };
+
+      function getUrl(environment) {
+        return environments[environment].url;
+      }
+
+      function getLoginUrl(environment) {
+        return getUrl(environment) + '/#/login';
+      }
+
+      function getLogourUrl(environment) {
+        return getUrl(environment) + '/#/logout';
+      }
+
+      function getTokensAPI(environment) {
+        return environments[environment].tokenservice + '/tokens';
+      }
+
+      return {
+        getLoginUrl: getLoginUrl,
+        getLogoutUrl: getLogourUrl,
+        getTokensAPI: getTokensAPI
+      }
+    }
+  ]);
