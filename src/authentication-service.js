17ee75c416fde92bae99140f78e2eb493b94699d
Damián García <damian.garcia@zalando.de>
2016-04-15 12:18:46 +0200

    Handle authentication with promises

--- src/authentication-service.js
+++ src/authentication-service.js
@@ -1,97 +1,98 @@
 angular.module('sc-authentication', ['ngStorage', 'ngCookies', 'angular-jwt'])
-  .factory('authenticationService', [
-    '$q',
-    '$localStorage',
-    '$cookies',
-    'environments',
-    '$location',
-    '$injector',
-    'jwtHelper',
-    function ($q, $localStorage, $cookies, environments, $location, $injector, jwtHelper) {
+    .factory('authenticationService', [
+      '$q',
+      '$localStorage',
+      '$cookies',
+      'environments',
+      '$location',
+      '$injector',
+      'jwtHelper',
+      function ($q, $localStorage, $cookies, environments, $location, $injector, jwtHelper) {
 
-      'use strict';
+        'use strict';
 
-      var TOKEN_COOKIE_KEY = "SC_TOKEN";
+        var TOKEN_COOKIE_KEY = "SC_TOKEN";
 
-      function authenticate(environment, redirectUrl) {
-        var token = isAuthenticated();
+        function authenticate(environment, redirectUrl) {
+          isAuthenticated()
+              .then(
+                  function (token) {
+                    // TODO login Only write if it's different than current value
+                    $localStorage.sc_token = token;
+                    $localStorage.sc_user = getUserFromToken(token);
 
-        if (!token) {
-          redirectToLogin(environment, redirectUrl);
+                    return token;
+                  },
+                  function () {
+                    redirectToLogin(environment, redirectUrl);
+                  });
         }
 
-        // TODO login Only write if it's different than current value
-
-        $localStorage.sc_token = token;
-        $localStorage.sc_user = getUserFromToken(token);
-
-        return token;
-      }
-
-      function redirectToLogin(environment, redirectUrl) {
-        $location.url(environments.getLoginUrl(environment) + "?redirect=" + redirectUrl);
-      }
-
-      function logout(environment) {
-        $localStorage.sc_token = null;
-        $localStorage.sc_user = null;
-
-        $location.url(environments.getLogoutUrl(environment));
-      }
-
-      function getToken() {
-        return $localStorage.sc_token || $cookies.get(TOKEN_COOKIE_KEY);
-      }
-
-      function getUser() {
-        return $localStorage.sc_user;
-      }
-
-      /*
-       PRIVATE METHODS
-       */
-
-      /**
-       * Returns a valid token in case the user is authenticated or null in case there's no user
-       * @returns {*}
-       */
-      function isAuthenticated() {
-        var token = getToken();
-        return validateToken(token).then(
-          function () {
-            // TODO remove - should never happen?
-            return token;
-          },
-          function (response) {
-            // TODO test this
-            if (response.status === 304) {
-              return token;
-            }
-            if (response.status === 409) {
-              return response.data;
-            }
-            return null;
-          });
-      }
-
-      function validateToken(token) {
-        if (!token) {
-          return $q.reject("null token");
+        function redirectToLogin(environment, redirectUrl) {
+          $location.url(environments.getLoginUrl(environment) + "?redirect=" + redirectUrl);
         }
 
-        return $injector.get('$http').get(environments.getTokensAPI(), token);
-      }
+        function logout(environment) {
+          $localStorage.sc_token = null;
+          $localStorage.sc_user = null;
 
-      function getUserFromToken(token) {
-        return jwtHelper.decodeToken(token);
-      }
+          $location.url(environments.getLogoutUrl(environment));
+        }
 
-      return {
-        authenticate: authenticate,
-        redirectToLogin: redirectToLogin,
-        getToken: getToken,
-        logout: logout,
-        getUser: getUser
-      };
-    }
-  ]);
+        function getToken() {
+          return $localStorage.sc_token || $cookies.get(TOKEN_COOKIE_KEY);
+        }
+
+        function getUser() {
+          return $localStorage.sc_user;
+        }
+
+        /*
+         PRIVATE METHODS
+         */
+
+        /**
+         * Returns a valid token in case the user is authenticated or null in case there's no user
+         * @returns {*}
+         */
+        function isAuthenticated() {
+          var token = getToken();
+          return validateToken(token).then(
+              function () {
+                // TODO remove - should never happen?
+                return $q.when(token);
+              },
+              function (response) {
+                // TODO test this
+                if (response.status === 304) {
+                  return $q.when(token);
+                }
+                if (response.status === 409) {
+                  var newToken = response.data;
+                  return $q.when(newToken);
+                }
+                return $q.reject();
+              });
+        }
+
+        function validateToken(token) {
+          if (!token) {
+            return $q.reject("null token");
+          }
+
+          return $injector.get('$http').get(environments.getTokensAPI(), token);
+        }
+
+        function getUserFromToken(token) {
+          return jwtHelper.decodeToken(token);
+        }
+
+        return {
+          authenticate: authenticate,
+          redirectToLogin: redirectToLogin,
+          getToken: getToken,
+          logout: logout,
+          getUser: getUser
+        };
+      }
+    ]);
