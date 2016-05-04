b9037c61316439efeaf5ebe76d892b65b0eeecca
Damián García <damian.garcia@zalando.de>
2016-05-04 10:54:33 +0200

    Update endpoints to new URL convention across Brand Solutions

--- src/environments-constant.js
+++ src/environments-constant.js
@@ -3,27 +3,32 @@ angular.module('sc-authentication')
       PRODUCTION: {
         url: 'https://www.solutions.zalando.com',
         tokenservice: 'https://token-management.norris.zalan.do',
+        userservice: 'https://user-management.norris.zalan.do',
         domain: 'solutions.zalando.com'
-      },
-      INTEGRATION: {
-        url: 'https://sc-integration.norris.zalan.do',
-        tokenservice: 'https://tm-integration.norris.zalan.do',
-        domain: '.zalan.do'
       },
       STAGE: {
         url: 'https://sc-stage.norris.zalan.do',
         tokenservice: 'https://tm-stage.norris.zalan.do',
+        userservice: 'https://um-stage.norris.zalan.do',
+        domain: '.zalan.do'
+      },
+      INTEGRATION: {
+        url: 'https://sc-integration.norris.zalan.do',
+        tokenservice: 'https://tm-integration.norris.zalan.do',
+        userservice: 'https://um-integration.norris.zalan.do',
         domain: '.zalan.do'
       },
       DEVELOPMENT: {
         url: 'https://sc-development.norris.zalan.do',
-        tokenservice: 'https://tm-dev-ext.norris.zalan.do',
+        tokenservice: 'https://tm-development.norris.zalan.do',
+        userservice: 'https://um-development.norris.zalan.do',
         domain: '.zalan.do'
       },
       LOCAL: {
         url: 'http://localhost:{PORT}',
         port: 3000,
-        tokenservice: 'https://tm-dev-ext.norris.zalan.do',
+        tokenservice: 'https://tm-development.norris.zalan.do',
+        userservice: 'https://um-development.norris.zalan.do',
         domain: 'localhost'
       }
     });
